3a685b24c620fbe67d8962641f8a9e21fd7e7b0d
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-04 19:34:46 +0000

    Add initial JavaScript practice questions and package-lock file

---
+++ javascript/ques.js
@@ -0,0 +1,22 @@
+// Javascript practice questions 1
+let a = "Aaryan"
+let b = "Budakoti"
+let c = a + " " + b
+console.log(c)
+
+
+console.log(c.length);
+console.log(c[0]);
+
+console.log(a[a.length-1]); //last letter
+
+pass = "aaryan"+ 2511; 
+console.log(pass);
+
+a= ""
+b=" "
+console.log(a.length); //length = 0
+console.log(b.length); //length = 1
+
+
+
