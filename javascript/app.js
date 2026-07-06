c086e49f46e1f1b3d187f58417fedf47ca00bbb3
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-06 11:55:11 +0000

    Refactor app.js to uncomment console.log and add traffic light logic

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,11 @@
-console.log("hellow worlddddd");
+// console.log("hellow worlddddd");
+let color = "green"
+if(color=="red"){
+    console.log("stop!")
+}
+if(color=="yellow"){
+    console.log("wait!")
+}
+if(color=="green"){
+    console.log("Go!")
+}
