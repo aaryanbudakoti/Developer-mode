1ff95758b3f2f2d33ed0cea6eeb95b3ed7fe5072
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-08 11:05:50 +0000

    Add switch statement to determine the day of the week

---
+++ javascript/day.js
@@ -0,0 +1,29 @@
+// Find which day it is , using Switch statement
+// monday = 1, tuesday =2 ....so on
+day = 2
+switch(day){
+    case 1:
+        console.log("It is Monday");
+        break;
+    case 2:
+        console.log("It is Tuesday");
+        break;
+    case 3:
+        console.log("It is Wednesday");
+        break;
+    case 4:
+        console.log("It is Thursday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7:
+        console.log("It is Sunday");
+        break;    
+    default:
+        console.log("Invalid Day");
+        
+    }
