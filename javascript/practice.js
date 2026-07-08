0b0d398c2e0f6e0d7d910cc34a60898ded1eab6e
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-08 11:08:21 +0000

    Comment out goodString validation logic in practice.js

--- javascript/practice.js
+++ javascript/practice.js
@@ -1,8 +1,8 @@
-let goodString = "aar";
+// let goodString = "aar";
 
-if (goodString[0] === "a" && goodString.length > 3){
-    console.log("It is a Good String");
-}
-else{
-    console.log("It is not a Good String");
-}
+// if (goodString[0] === "a" && goodString.length > 3){
+//     console.log("It is a Good String");
+// }
+// else{
+//     console.log("It is not a Good String");
+// }
