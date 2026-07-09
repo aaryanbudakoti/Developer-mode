1d3e6edb1b84f661ddfa76de40ae0c4e95bd3219
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-09 09:35:58 +0000

    Add initial string.html and strings.js files with basic content

---
+++ javascript/strings.js
@@ -0,0 +1,19 @@
+// str = "            he   llo          ";
+// console.log(str.trim());
+// let pass = prompt("set your pass ");
+// console.log(password.trim());
+
+// strings are immutable in JS
+// whenever we do try to make a change, a new string is created with the changes
+str = "aaryanbudakoti";
+console.log(str.toUpperCase());
+
+
+// string methods with arguments
+// stringname.method(arg)
+
+
+str.indexOf("ryan")
+// -1 if not found
+
+let msg = "I love chai"
