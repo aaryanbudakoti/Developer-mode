d6550cfc826744e330ecb157e7bf5bd0aef5e6c0
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-12 19:19:36 +0000

    Refactor array.js to enhance array method examples and improve code clarity

--- javascript/array.js
+++ javascript/array.js
@@ -1,3 +1,16 @@
 cars=["Maruti","Ferrari","Aston Martin","Lamborghini"]
-
+// array methods - push,pop,shift,unshift
 // Practice Question
+arr=['january','july','march','august'];
+arr.shift();
+arr.shift();
+arr.unshift('june');
+arr.unshift('july');
+// console.log(arr);
+// solution arr = ['july','june','march','august']
+
+
+// indexOf and include method
+cars.indexOf('Maruti');
+cars.indexOf("Ferrari");
+console.log(cars.includes('Audi'));  // false
