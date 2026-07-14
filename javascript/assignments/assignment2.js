db398baeafa4d7fda1cbcff3d9ddd0c52a41a230
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-14 10:06:36 +0000

    Add initial implementation of assignment2.js with various array and string operations

---
+++ javascript/assignments/assignment2.js
@@ -0,0 +1,40 @@
+// Q1
+arr = [7,9,0,-2];
+let n=3;
+// console.log(arr.slice(0,n))
+
+// Q2
+arr = [7,9,0,-2];
+n=3;
+console.log(arr.slice(arr.length-n))
+
+
+
+// Q3
+str = "aaryanbudakoti";
+console.log(str.length == 0);
+
+// Q4
+let string = "Ikka"
+let idx = 3;
+if (string[idx]==string[idx].toLowerCase){
+    console.log("character is in lowercase")
+
+}
+else{
+    console.log("character is not in lowercase")
+}
+
+//Q5
+a = "    dr. abj abdul kalam    "
+console.log(a.trim())
+
+
+// Q6
+let ray = [1,2,3,4,5,55];
+let element = 10;
+console.log(ray.indexOf(element))
+
+
+
+
