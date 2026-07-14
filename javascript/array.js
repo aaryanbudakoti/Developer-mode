979ad3b3b7477c793d363c5fb44e077547c896be
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-14 08:47:30 +0000

    Comment out console log examples for concat and reverse methods in array.js

--- javascript/array.js
+++ javascript/array.js
@@ -20,10 +20,24 @@ console.log(cars.includes('Audi'));  // false
 
 let primary = [ "red","yellow","blue"]
 let secondary = ["orange","green","violet"]
-console.log(primary.concat(secondary));
-console.log(primary)
+// console.log(primary.concat(secondary));
+// console.log(primary)
 let allColors = primary.concat(secondary)
-console.log(allColors)
+// console.log(allColors)
 
 
-console.log(primary.reverse())
+// console.log(primary.reverse())
+
+// splice - mmake changes in original array (start,deletecount, replace element)
+
+let colors = ["red","pink","white", "blue"," orange" ]
+console.log(colors.splice(3))
+console.log(colors)
+
+console.log(colors.splice(0,1))
+console.log(colors)
+
+console.log(colors.splice(0,1,"black"))
+console.log(colors)
+
+
