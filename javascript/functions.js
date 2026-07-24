4fbdcb921a8bf5623a946a4e2fc11d243f613ba2
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-24 10:50:46 +0000

    function examples and questions

--- javascript/functions.js
+++ javascript/functions.js
@@ -28,7 +28,65 @@ function rollDice(){
     // generates numbers 1 to 6
     console.log(Math.floor(Math.random()*6)+1)
 }
-rollDice(); // function calling
+// rollDice(); // function calling
 
 
 // Function with arguments
+function add(a,b){
+    console.log(a+b);
+}
+
+// add(1212,1212); //2424
+
+function printyourName(Name){
+    console.log(Name)
+}
+
+// printyourName("niggapur");
+
+// Difference between paramter and argument :
+
+// parameter is a variable in the declaration of function
+
+// argument is the actual value of this variable that gets passed to function
+
+// avg of 3 numbers
+function avg(num1,num2,num3){
+    let av=(num1+num2+num3)/3
+    console.log(av);
+}
+
+// avg(10,12,15);
+
+
+// multiplication table function
+function mulTable(num){
+    for(let i=1; i<=10;i++){
+        console.log(num*i);
+
+    }
+}
+// mulTable(73);
+// sum of n numbers (1 to n)
+function sumN(number){
+    let sum = 0
+    for(let i =1;i<=number;i++){
+         sum+=i ;
+    }
+    return sum;
+}
+
+// console.log(sumN(1000));
+
+
+// 
+let arr = [ "hi ", "hello ", "what are you doing?"]
+function concat(str){
+    let result = "";
+    for(let i = 0;i<str.length;i++){
+        result+= str[i];
+    }
+    return result;
+}
+console.log(concat(arr))
+
