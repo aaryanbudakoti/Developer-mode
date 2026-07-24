c42a2151f973f42c7bcfe2d113d92dc4d1db18f1
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-07-24 10:10:35 +0000

    js function programs

--- javascript/functions.js
+++ javascript/functions.js
@@ -6,4 +6,29 @@ function hello(){
 }
 
 // function call
-hello();
+// hello();
+
+function printName(){
+    console.log("my Name is Aaryan Budakoti")
+
+}
+
+// printName();
+
+function poem(){
+    console.log("Baba black sheep have you any wool?")
+    console.log("yes sir, yes sir, three bags full!")
+
+}
+// poem();
+
+
+// dice 1-6
+function rollDice(){
+    // generates numbers 1 to 6
+    console.log(Math.floor(Math.random()*6)+1)
+}
+rollDice(); // function calling
+
+
+// Function with arguments
