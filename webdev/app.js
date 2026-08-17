47b567a43f04f809c252b8cc568ba53e8daae932
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-08-17 11:08:47 +0000

    Remove commented-out console.dir statements for improved code clarity

--- webdev/app.js
+++ webdev/app.js
@@ -4,3 +4,12 @@
 //     smallImages[i].src = "assets/spiderman_img.png"
 //     console.log(`Value of image no. ${i} is changed`);
 // }
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+
+// console.dir(document.querySelector("div a"));
+
+console.dir(document.querySelectorAll("div a"));
+console.dir(document.querySelectorAll(" p "));
