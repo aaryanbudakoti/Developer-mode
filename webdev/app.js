23713efe847c5ea6ca4f171bbd4ce5752342a403
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-08-18 17:12:24 +0000

    Comment out unused DOM manipulation code for clarity

--- webdev/app.js
+++ webdev/app.js
@@ -23,29 +23,46 @@
 //     link.style.color = "red";
 // }
 
-let para1 = document.createElement('p');
-para1.innerText = "Hey, I am red" ; 
-document.querySelector('body').append(para1);
+// let para1 = document.createElement('p');
+// para1.innerText = "Hey, I am red" ; 
+// document.querySelector('body').append(para1);
 
-para1.classList.add("red")
+// para1.classList.add("red")
 
-let h3 = document.createElement("h3");
-h3.innerText = "im a blue h3 !";
-document.querySelector("body").append(h3);
+// let h3 = document.createElement("h3");
+// h3.innerText = "im a blue h3 !";
+// document.querySelector("body").append(h3);
 
-h3.classList.add('blue');
+// h3.classList.add('blue');
 
 
-let div = document.createElement("div");
-let h1 = document.createElement("h1");
-let para2 = document.createElement("p");
+// let div = document.createElement("div");
+// let h1 = document.createElement("h1");
+// let para2 = document.createElement("p");
 
-h1.innerText = " I am in a div"
-para2.innerText = "me too!!!"
+// h1.innerText = " I am in a div"
+// para2.innerText = "me too!!!"
 
-div.append(h1);
-div.append(para2);
+// div.append(h1);
+// div.append(para2);
 
-div.classList.add("box")
+// div.classList.add("box")
 
-document.querySelector("body").append(div)
+// document.querySelector("body").append(div)
+
+
+// Assignment 
+// 1
+
+let input = document.createElement("input");
+let button = document.createElement("button");
+
+button.innerText = "Click me"
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+// 2
+
+button.setAttribute("id","btn");
+input.setAttribute("placeholder","username");
