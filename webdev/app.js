33344d8ca8735116b9c3af0f3ce85a755b7e9292
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-08-18 11:47:59 +0000

    using DOM to give solution to assignment

--- webdev/app.js
+++ webdev/app.js
@@ -1,24 +1,51 @@
-// let smallImages = document.getElementsByClassName("oldImg");
+// // let smallImages = document.getElementsByClassName("oldImg");
 
-// for(let i =0;i<smallImages.length ; i++){
-//     smallImages[i].src = "assets/spiderman_img.png"
-//     console.log(`Value of image no. ${i} is changed`);
+// // for(let i =0;i<smallImages.length ; i++){
+// //     smallImages[i].src = "assets/spiderman_img.png"
+// //     console.log(`Value of image no. ${i} is changed`);
+// // }
+
+// // console.dir(document.querySelector("h1"));
+// // console.dir(document.querySelector("#description"));
+// // console.dir(document.querySelector(".oldImg"));
+
+// // console.dir(document.querySelector("div a"));
+
+// // console.dir(document.querySelectorAll("div a"));
+// // console.dir(document.querySelectorAll(" p "));
+// let links = document.querySelectorAll('.box a');
+
+// // for(let i =0 ; i<links.length ; i++){
+// //     links[i].style.color = "green";
+// // }
+
+// for(link of links){
+//     link.style.color = "red";
 // }
 
-// console.dir(document.querySelector("h1"));
-// console.dir(document.querySelector("#description"));
-// console.dir(document.querySelector(".oldImg"));
+let para1 = document.createElement('p');
+para1.innerText = "Hey, I am red" ; 
+document.querySelector('body').append(para1);
 
-// console.dir(document.querySelector("div a"));
+para1.classList.add("red")
 
-// console.dir(document.querySelectorAll("div a"));
-// console.dir(document.querySelectorAll(" p "));
-let links = document.querySelectorAll('.box a');
+let h3 = document.createElement("h3");
+h3.innerText = "im a blue h3 !";
+document.querySelector("body").append(h3);
 
-// for(let i =0 ; i<links.length ; i++){
-//     links[i].style.color = "green";
-// }
+h3.classList.add('blue');
 
-for(link of links){
-    link.style.color = "red";
-}
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
+
+h1.innerText = " I am in a div"
+para2.innerText = "me too!!!"
+
+div.append(h1);
+div.append(para2);
+
+div.classList.add("box")
+
+document.querySelector("body").append(div)
