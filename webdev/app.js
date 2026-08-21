649022a6d6adf0d969e54494ff75b3e52704970d
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-08-21 11:05:59 +0000

    Refactor app.js and index.html for clarity; remove unused code and comments

--- webdev/app.js
+++ webdev/app.js
@@ -1,68 +1,12 @@
-// // let smallImages = document.getElementsByClassName("oldImg");
+let btn = document.querySelector("button");
+console.dir(btn)
 
-// // for(let i =0;i<smallImages.length ; i++){
-// //     smallImages[i].src = "assets/spiderman_img.png"
-// //     console.log(`Value of image no. ${i} is changed`);
-// // }
-
-// // console.dir(document.querySelector("h1"));
-// // console.dir(document.querySelector("#description"));
-// // console.dir(document.querySelector(".oldImg"));
-
-// // console.dir(document.querySelector("div a"));
-
-// // console.dir(document.querySelectorAll("div a"));
-// // console.dir(document.querySelectorAll(" p "));
-// let links = document.querySelectorAll('.box a');
-
-// // for(let i =0 ; i<links.length ; i++){
-// //     links[i].style.color = "green";
-// // }
-
-// for(link of links){
-//     link.style.color = "red";
+// btn.onclick = function(){
+//     alert("button was clicked");
 // }
 
-// let para1 = document.createElement('p');
-// para1.innerText = "Hey, I am red" ; 
-// document.querySelector('body').append(para1);
+function sayHello(){
+    alert("Hello")
+}
 
-// para1.classList.add("red")
-
-// let h3 = document.createElement("h3");
-// h3.innerText = "im a blue h3 !";
-// document.querySelector("body").append(h3);
-
-// h3.classList.add('blue');
-
-
-// let div = document.createElement("div");
-// let h1 = document.createElement("h1");
-// let para2 = document.createElement("p");
-
-// h1.innerText = " I am in a div"
-// para2.innerText = "me too!!!"
-
-// div.append(h1);
-// div.append(para2);
-
-// div.classList.add("box")
-
-// document.querySelector("body").append(div)
-
-
-// Assignment 
-// 1
-
-let input = document.createElement("input");
-let button = document.createElement("button");
-
-button.innerText = "Click me"
-
-document.querySelector("body").append(input);
-document.querySelector("body").append(button);
-
-// 2
-
-button.setAttribute("id","btn");
-input.setAttribute("placeholder","username");
+btn.onclick = sayHello; 
