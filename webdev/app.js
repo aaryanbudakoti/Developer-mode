12b84712f9def2f0ec1a04c49673667516ee203d
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-08-25 16:24:19 +0000

    Refactor app.js and index.html to demonstrate 'this' in event listeners; update HTML structure for clarity

--- webdev/app.js
+++ webdev/app.js
@@ -1,42 +1,52 @@
-let btns = document.querySelectorAll("button");
-console.dir(btns)
+// let btns = document.querySelectorAll("button");
+// console.dir(btns)
 
-// btn.onclick = function(){
-//     alert("button was clicked");
+// // btn.onclick = function(){
+// //     alert("button was clicked");
+// // }
+
+// function sayHello(){
+//     alert("Hello")
+// }
+// function sayName(){
+//     alert("My name is Aaryan")
+// }
+// // btn.onclick = sayHello; 
+// // onclick can execute only a single function 
+
+
+// // Event Listener
+// // element.addEventListener(event,callback fxn)
+// for (btn of btns){
+
+//     // btn.addEventListener("click",sayHello);
+//     // btn.addEventListener("click",sayName);
+//     btn.addEventListener("dblclick",function (){
+//         console.log("you double clicked the button bro!")
+//     });
+
 // }
 
-function sayHello(){
-    alert("Hello")
-}
-function sayName(){
-    alert("My name is Aaryan")
-}
-// btn.onclick = sayHello; 
-// onclick can execute only a single function 
+// // There are many other events
+// // Animations , mouse Events (auxclick, click, contextmenu etc.)
+
+// let p = document.querySelector("p");
+
+// p.addEventListener("click", function (){
+//     console.log("paragraph was clicked");
+// })
+
+// let box = document.querySelector(".box");
+// box.addEventListener("mouseenter", function () {
+//     console.log("mouse inside box");
+// });
 
 
-// Event Listener
-// element.addEventListener(event,callback fxn)
-for (btn of btns){
+// this in event listener
+let btn = document.querySelector("button");
 
-    // btn.addEventListener("click",sayHello);
-    // btn.addEventListener("click",sayName);
-    btn.addEventListener("dblclick",function (){
-        console.log("you double clicked the button bro!")
-    });
-
-}
-
-// There are many other events
-// Animations , mouse Events (auxclick, click, contextmenu etc.)
-
-let p = document.querySelector("p");
-
-p.addEventListener("click", function (){
-    console.log("paragraph was clicked");
-})
-
-let box = document.querySelector(".box");
-box.addEventListener("mouseenter", function () {
-    console.log("mouse inside box");
+btn.addEventListener("click", function(){
+    console.dir(this.innerText)
+    this.style.backgroundColor = "blue";
+    this.style.color = "white";
 });
